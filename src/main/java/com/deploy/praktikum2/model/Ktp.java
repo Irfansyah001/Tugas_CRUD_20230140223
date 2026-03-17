ce32395d0975d6944683551988930d4827c414d8
汉堡包 <syahirfan545@gmail.com>
2026-03-18 02:43:35 +0700

    Add `Ktp` entity and complete KTP management frontend structure
    - Introduce `Ktp` entity class with JPA annotations.
    - Add `index.html` for managing KTP data in the frontend.

---
+++ src/main/java/com/deploy/praktikum2/model/Ktp.java
@@ -0,0 +1,37 @@
+package com.deploy.praktikum2.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ktp")
+public class Ktp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String nomorKtp;
+
+    @Column(nullable = false)
+    private String namaLengkap;
+
+    @Column(nullable = false)
+    private String alamat;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @Column(nullable = false)
+    private LocalDate tanggalLahir;
+
+    @Column(nullable = false)
+    private String jenisKelamin;
+}
