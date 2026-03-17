aab20823d42a0bec1623c2c6bace6a963d841965
汉堡包 <syahirfan545@gmail.com>
2026-03-18 02:43:55 +0700

    Add `KtpRepository` for database operations on KTP entities

---
+++ src/main/java/com/deploy/praktikum2/repository/KtpRepository.java
@@ -0,0 +1,15 @@
+package com.deploy.praktikum2.repository;
+
+import com.deploy.praktikum2.model.Ktp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface KtpRepository extends JpaRepository<Ktp, Integer> {
+
+    Optional<Ktp> findByNomorKtp(String nomorKtp);
+
+    boolean existsByNomorKtp(String nomorKtp);
+}
